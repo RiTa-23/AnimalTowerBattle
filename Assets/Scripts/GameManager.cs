61803295e378e307128cd99a4eda5d3b9d545c32
RiTa-23 <rita.tifrt@gmail.com>
2025-07-14 14:34:58 +0900

    add:GameManager追加

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,85 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] GameObject[] characters;//キャラクターオブジェクト配列
+    bool isGene = false;//キャラクターが生成されたかどうか
+    GameObject geneChara; //生成されたキャラクター単体
+    bool isButtonHover = false; //ボタンがホバーされているかどうか
+    bool isInterval = false; //キャラクター生成の間隔を制御
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        //キャラクターが生成されていないかつキャラが静止している場合
+        if (!isGene && !CheckMove()&&!isInterval)
+        {
+            CreateCharacter(); //キャラクターを生成
+            isGene = true;
+        }
+        //マウスの左ボタンが離されたとき、かつキャラクターが生成されている場合
+        else if (Input.GetMouseButtonUp(0) && isGene)
+        {
+            if (!isButtonHover)
+            {
+                //物理挙動を有効にする
+                geneChara.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+                isGene = false; //キャラクター生成フラグをリセット
+                StartCoroutine(IntervalCoroutine()); //キャラクター生成の間隔を制御
+            }
+        }
+        else if (Input.GetMouseButton(0) && isGene && !isButtonHover)
+        {
+            //マウスの左ボタンが押されている間、キャラクターを移動させる(x座標のみ)
+            float mousePositionX = Camera.main.ScreenToWorldPoint(Input.mousePosition).x;
+            geneChara.transform.position = new Vector2(mousePositionX, transform.position.y);
+        }
     }
+    void CreateCharacter()
+    {
+        //回転せずにGameManagerの座標にランダムにキャラ生成
+        geneChara = Instantiate(characters[Random.Range(0, characters.Length)],
+        transform.position, Quaternion.identity);
+        //物理挙動をさせない状態にする
+        geneChara.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
+    }
+
+    bool CheckMove()
+    {
+        //Characterタグのオブジェクトを取得
+        GameObject[] characterObjects = GameObject.FindGameObjectsWithTag("Character");
+        foreach (GameObject character in characterObjects)
+        {
+            //キャラクターの速度が0.01以上なら動いていると判断
+            if (character.GetComponent<Rigidbody2D>().linearVelocity.magnitude > 0.01f)
+            {
+                return true; //キャラクターが動いている場合はtrue
+            }
+        }
+        return false; //キャラクターが動いていない場合はfalse
+    }
+
+    IEnumerator IntervalCoroutine()
+    {
+        isInterval = true; //間隔制御フラグを立てる
+        yield return new WaitForSeconds(1f); //1秒待機
+        isInterval = false; //間隔制御フラグをリセット
+    }
+
+    public void RotateCharacter()
+    {
+        if (isGene)
+            geneChara.transform.Rotate(0, 0, -30);//30度ずつ回転
+    }
+
+    public void IsButtonChange(bool isX)
+    {
+        isButtonHover = isX; //ボタンの状態を変更
+    }
+    
 }
